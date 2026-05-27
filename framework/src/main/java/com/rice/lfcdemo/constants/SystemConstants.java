fed4512091cb2cc50f32e9d336a6d71f9d75c911
fclttl <770995260@qq.com>
2026-05-27 21:21:03 +0800

    [ADD]增加常量

--- framework/src/main/java/com/rice/lfcdemo/constants/SystemConstants.java
+++ framework/src/main/java/com/rice/lfcdemo/constants/SystemConstants.java
@@ -3,4 +3,6 @@ package com.rice.lfcdemo.constants;
 public class SystemConstants {
 
     public static final int ARTICLE_STATUS_NORMAL = 0;
+
+    public static final String STATUS_NORMAL = "0";
 }
