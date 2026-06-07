560e7332a773bd44b32971670306e5a087c6edec
fclttl <770995260@qq.com>
2026-06-07 10:09:36 +0800

    [ADD] 新增消息推送系统相关配置和文件夹

--- xtimer/src/test/java/com/rice/lfcdemo/service/rabbitMq/MessageProducerTest.java
+++ xtimer/src/test/java/com/rice/lfcdemo/service/rabbitMq/MessageProducerTest.java
@@ -4,8 +4,10 @@ import com.rice.lfcdemo.entity.Xtimer;
 import com.rice.lfcdemo.entity.dto.TimerDTO;
 import com.rice.lfcdemo.entity.param.NotifyHTTPParam;
 import com.rice.lfcdemo.mapper.XtimerMapper;
+import com.rice.lfcdemo.service.CleanCache.CleanCacheJob;
 import com.rice.lfcdemo.utils.TimerUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,9 +20,20 @@ class MessageProducerTest {
     private MessageProducer producer;
     @Autowired
     private XtimerMapper xtimerMapper;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private CleanCacheJob cleanCacheJob;
+
     @Test
     void sendMessage() {
         String msg = TimerUtils.UnionTimerIDUnix(8000, 20260605);
         producer.sendMessage(msg);
+    }
+
+    @Test
+    void cleanCache() {
+        String test = "2026-06-05 22:*";
+        cleanCacheJob.cleanCache();
     }
 }
