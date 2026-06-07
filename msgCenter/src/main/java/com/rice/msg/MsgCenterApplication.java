e682e61593cb9d8515f4f454b409518267d5aa8c
fclttl <770995260@qq.com>
2026-06-07 10:09:36 +0800

    [ADD] 新增消息推送系统相关配置和文件夹

---
+++ msgCenter/src/main/java/com/rice/msg/MsgCenterApplication.java
@@ -0,0 +1,11 @@
+package com.rice.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsgCenterApplication.class, args);
+    }
+}
