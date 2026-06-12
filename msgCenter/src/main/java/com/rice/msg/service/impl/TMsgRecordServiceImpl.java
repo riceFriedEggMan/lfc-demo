1c570aced79ff71eeae73ada0611ce2b78e823c7
fclttl <770995260@qq.com>
2026-06-12 20:12:36 +0800

    [ADD] 增加查询消息接口

--- msgCenter/src/main/java/com/rice/msg/service/impl/TMsgRecordServiceImpl.java
+++ msgCenter/src/main/java/com/rice/msg/service/impl/TMsgRecordServiceImpl.java
@@ -1,14 +1,23 @@
 package com.rice.msg.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.rice.lfcdemo.model.ResponseEntity;
+import com.rice.msg.common.conf.SendMsgConf;
+import com.rice.msg.constant.Constants;
 import com.rice.msg.entity.TMsgRecord;
 import com.rice.msg.entity.TMsgTemplate;
 import com.rice.msg.enums.MsgStatus;
 import com.rice.msg.mapper.TMsgRecordMapper;
+import com.rice.msg.model.MsgRecordModel;
 import com.rice.msg.model.dto.SendMsgReq;
 import com.rice.msg.service.TMsgRecordService;
 import com.rice.msg.utils.JSONUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 /**
  * 消息记录表(TMsgRecord)表服务实现类
@@ -18,6 +27,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TMsgRecordServiceImpl extends ServiceImpl<TMsgRecordMapper, TMsgRecord> implements TMsgRecordService {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+    @Autowired
+    private SendMsgConf sendMsgConf;
 
     @Override
     public void createMsgRecord(String msgId, SendMsgReq sendMsgReq, TMsgTemplate tp, MsgStatus msgStatus) {
@@ -36,5 +49,24 @@ public class TMsgRecordServiceImpl extends ServiceImpl<TMsgRecordMapper, TMsgRec
             log.error("存储消息发送记录失败， msgId:"+tMsgRecord.getMsgId());
         }
 
+    }
+
+    @Override
+    public ResponseEntity getMessageById(String msgId) {
+        String key = Constants.REDIS_KEY_MES_RECORD + msgId;
+        String object = redisTemplate.opsForValue().get(key);
+        MsgRecordModel msgRecordModel = null;
+        if (StringUtils.isNotBlank(object) && sendMsgConf.isOpenCache()) {
+            msgRecordModel = JSONUtil.parseObject(object, MsgRecordModel.class);
+            if (msgRecordModel != null) {
+                return ResponseEntity.ok(msgRecordModel);
+            }
+        }
+
+        msgRecordModel = getBaseMapper().getMsgById(msgId);
+
+        redisTemplate.opsForValue().set(key, JSONUtil.toJsonString(msgRecordModel), Duration.ofSeconds(30));
+
+        return ResponseEntity.ok(msgRecordModel);
     }
 }
